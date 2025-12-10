2c4c5d43599a6db9a24aa83e1114d8069fc2ea22
Baher Mohammad <bahermh19@gmail.com>
2025-12-10 03:54:15 +0300

    load env in conf

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 
+load_dotenv()
 # ---------- PATHS ----------
 
 # Folder where you will manually put downloaded AWS billing docs
